42bf4434f466b352c78b08134e2aecfbf4786609
Kevingg06 <kevingonzalezr433@gmail.com>
2024-04-22 23:10:54 -0300

    a

--- app/src/main/java/com/example/ejerciciodeprogramacinorientadaaobjetospoo/DeleteActivity.kt
+++ app/src/main/java/com/example/ejerciciodeprogramacinorientadaaobjetospoo/DeleteActivity.kt
@@ -5,26 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.ejerciciodeprogramacinorientadaaobjetospoo.databinding.ActivityMainBinding
+import com.example.ejerciciodeprogramacinorientadaaobjetospoo.databinding.ActivityDeleteBinding
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityMainBinding
+class DeleteActivity : AppCompatActivity() {
+    lateinit var binding : ActivityDeleteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
+        binding = ActivityDeleteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-
     }
-
-
 }
