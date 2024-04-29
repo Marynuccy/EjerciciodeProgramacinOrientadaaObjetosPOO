d8ac4669a7393e3847b48feaf5c6d0ee0faa57e6
Marynuccy <tomas.marinucci06@gmail.com>
2024-04-29 16:28:30 -0300

    AddTablet

--- app/src/main/java/com/example/ejerciciodeprogramacinorientadaaobjetospoo/classes/Tablet.kt
+++ app/src/main/java/com/example/ejerciciodeprogramacinorientadaaobjetospoo/classes/Tablet.kt
@@ -1,5 +1,7 @@
 package com.example.ejerciciodeprogramacinorientadaaobjetospoo.classes
 
+import com.example.ejerciciodeprogramacinorientadaaobjetospoo.attributes.AttributesTablet
+
 class Tablet(brand: String, model: String, state: State, id: String, attributes : AttributesTablet) :
     Devices(brand, model, state, id) {
 
